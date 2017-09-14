7c349f56f6d74cbccb35a80a2d25cda4b97dc04e
Baelx <awintschel@gmail.com>
2017-09-14 15:47:10 -0400

    Updated with document.location.protocol

--- change-link-location.js
+++ change-link-location.js
@@ -1,4 +1,5 @@
 var nodeList = document.querySelectorAll('a[title="Title of a tag"]'); //Select the title of the link here - between the double-quotes
 nodeList.forEach(function(el){
-    el.href = "https://example.com"; //Input the link to go to here - make sure to use HTTPS
+    el.href = document.location.protocol + "//example.com"; //Input the link to go to here - omit the protocol in the link "https, https"
 });
+//Copy lines 1 to 4 and re-do for each language in the shop
