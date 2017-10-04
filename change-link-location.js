c743a6b6509b7c0d888489f2173f19855455fd81
Baelx <awintschel@gmail.com>
2017-10-04 17:25:16 -0400

    Update change-link-location.js

--- change-link-location.js
+++ change-link-location.js
@@ -1,4 +1,5 @@
-var nodeList = document.querySelectorAll('a[href="Old link of the a tag in question"]'); //Input the current href value of the a tag here, between the double-quotes
+var nodeList = document.querySelectorAll('a[href~="Old link of the a tag in question excluding the protol"]'); 
+//Input the current href value of the a tag here, between the double-quotes. Leave out the HTTP or HTTPS from the link you enter
 nodeList.forEach(function(el){
     el.href = document.location.protocol + "//example.com"; //Input the link to go to here - omit the protocol in the link "https, https"
 });
